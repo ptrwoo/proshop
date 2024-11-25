aa9679d50170a7a922d71e153a2881d22abd796c
ptrwoo <144814534+ptrwoo@users.noreply.github.com>
2024-11-25 16:26:06 -0500

    creating custom bootstrap theme now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
