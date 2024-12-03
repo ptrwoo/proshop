b6933230ee50a200706731213364cba24ef0bfcc
ptrwoo <144814534+ptrwoo@users.noreply.github.com>
2024-12-03 16:26:48 -0500

    added custom error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5001;
 
@@ -15,13 +17,16 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
