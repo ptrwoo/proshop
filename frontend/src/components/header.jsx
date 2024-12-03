4ce1d1536d0eaee9f554af7536c60830aba3c012
ptrwoo <144814534+ptrwoo@users.noreply.github.com>
2024-12-03 10:49:12 -0500

    fixed the bug no more react router bootstrap

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,32 +1,26 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
-import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="ProShop" />
-              ProShop
-            </Navbar.Brand>
-          </LinkContainer>{" "}
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="ProShop" />
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <FaShoppingCart /> Cart{" "}
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign In{" "}
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link as={Link} to="/">
+                <FaShoppingCart /> Cart{" "}
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                <FaUser /> Sign In{" "}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
